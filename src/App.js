2b746fac41915359edce7eb91e06e429f44a081c
Nejc <nejcrogelsek0@gmail.com>
2021-02-28 23:14:06 +0100

    integration completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
 function App() {
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "0.01",
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    return actions.order.capture();
+  };
   return (
     <>
-      <div className='app'>app</div>
+      <div className='app'>
+        <div className='wrapper'>
+          <PayPalButton
+            createOrder={(data, actions) => createOrder(data, actions)}
+            onApprove={(data, actions) => onApprove(data, actions)}
+          />
+        </div>
+      </div>
     </>
   );
 }
